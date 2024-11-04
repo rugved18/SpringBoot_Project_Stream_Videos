6373ed70084e343576ee29fa1ff50ccc0b5fe7a4
rugved18 <rugvedkulk2003@gmail.com>
2024-11-04 22:23:56 +0530

    04-11-2024

--- src/main/java/com/stream/Streaming_app/Controller/VideoController.java
+++ src/main/java/com/stream/Streaming_app/Controller/VideoController.java
@@ -1,13 +1,26 @@
 package com.stream.Streaming_app.Controller;
 
+import com.stream.Streaming_app.Service.VideosService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("Video")
+@RequestMapping("/api")
 public class VideoController {
 
-    @GetMapping("/output")
-    public String Output(){
-        return "Hello World This is Rugved";
+  public   VideosService videosService;
+
+   public VideoController(VideosService videosService){
+       this.videosService= videosService;
+    }
+
+    @PostMapping
+    public ResponseEntity<contextMessage> create(
+            @RequestParam("file")MultipartFile file,
+            @RequestParam("title")String title,
+            @RequestParam("discription") String discription
+            ){
+       
     }
 }
--- src/main/java/com/stream/Streaming_app/Service/VideosService.java
+++ src/main/java/com/stream/Streaming_app/Service/VideosService.java
@@ -1,26 +1,48 @@
 package com.stream.Streaming_app.Service;
 
 import com.stream.Streaming_app.Model.Videos;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class VideosService {
 
-    public Videos SaveVideo(Videos video, MultipartFile file){
-        String filename = file.getOriginalFilename();
+    @Value("${file.video}")
+    String DIR;
+
+    public Videos SaveVideo(Videos video, MultipartFile file) {
+        try {
+            String fileName = file.getOriginalFilename();
+            String fileContent = file.getContentType();
+            InputStream inputStream = file.getInputStream();
+
+            String cleanFilename = StringUtils.cleanPath(fileName);
+            String cleanFolder = StringUtils.cleanPath(DIR);
+
+            Path path = Paths.get(cleanFilename,cleanFolder);
+
+        } catch (IOException e) {
+            System.out.println("error and need to handle it");
+        }
         return null;
     }
 
-    public Videos Get(String VideoId){
+    public Videos Get(String VideoId) {
         return null;
     }
 
-    public Videos GetTitle(String VideoTitle){
+    public Videos GetTitle(String VideoTitle) {
         return null;
     }
 
-    public List<Videos> getAll(){
+    public List<Videos> getAll() {
         return null;
     }
 }
+
